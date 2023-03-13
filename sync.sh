a621c73e680bb7f3064be45805c597b918e96376
Sanjiv Sahayam <sanjsmailbox@gmail.com>
2023-03-13 13:29:29 +1100

    update sync and add data/dist to commit

--- sync.sh
+++ sync.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-docker compose run --rm --entrypoint 'bash -c "site clean && site rebuild && site deploy"' blog
+# docker compose run --rm --entrypoint 'bash -c "site clean && site rebuild"' blog
+
+# echo "Copying dist files into $BLOG_WEBSITE_DIR"
+
+rsync -av \
+      --checksum \
+      --delete \
+      --progress \
+      data/dist/_site/* $BLOG_WEBSITE_DIR
